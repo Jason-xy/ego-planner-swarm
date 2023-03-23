800b3a859f850df43debad8f66e3fe2991717c2a
jason-xy <1462357003@qq.com>
2023-03-23 13:14:13 +0800

    change mount dir in docker

--- scripts/compile/compile.sh
+++ scripts/compile/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script_dir=$(cd $(dirname $0);pwd)
-sudo docker run --rm \
-    -v $script_dir/../../:/root/ros1_ws/ \
+docker run --rm \
+    -v $script_dir/../../src:/root/ros1_ws/src \
     jasonxxxyyy/sky-explorer:runtime-cuda11.4-ros1-$1 \
     /bin/bash -c "cd /root/ros1_ws && \
             source /opt/ros/noetic/setup.bash && \
